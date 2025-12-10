7289e50f1fe9785ae0dc0b2aaf36180dee0b42d6
maricus <mo@myworkpay.com>
2025-12-10 22:10:49 +0300

    reservations update

--- apps/reservations/src/reservations.controller.ts
+++ apps/reservations/src/reservations.controller.ts
@@ -1,12 +1,52 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Logger,
+} from '@nestjs/common';
 import { ReservationsService } from './reservations.service';
+import { CreateReservationDto } from './dto/create-reservation.dto';
+import { UpdateReservationDto } from './dto/update-reservation.dto';
 
-@Controller()
+@Controller('reservations')
 export class ReservationsController {
+  private readonly logger = new Logger(ReservationsController.name);
   constructor(private readonly reservationsService: ReservationsService) {}
 
+  @Post()
+  create(@Body() createReservationDto: CreateReservationDto) {
+    this.logger.log('Creating reservation', createReservationDto);
+    return this.reservationsService.create(createReservationDto);
+  }
+
   @Get()
-  getHello(): string {
-    return this.reservationsService.getHello();
+  findAll() {
+    this.logger.log('Finding all reservations');
+    return this.reservationsService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    this.logger.log('Finding reservation', id);
+    return this.reservationsService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id') id: string,
+    @Body() updateReservationDto: UpdateReservationDto,
+  ) {
+    this.logger.log('Updating reservation', id, updateReservationDto);
+    return this.reservationsService.update(+id, updateReservationDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    this.logger.log('Removing reservation', id);
+    return this.reservationsService.remove(+id);
   }
 }
